f65ccdc7eea8008b11fb434be866b588f266fdb2
Coms <125128463+ComsIndeed@users.noreply.github.com>
2025-11-12 06:13:47 +0800

    Implement NoteWidget with title and content streams and add UI layout

--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class NoteWidget extends StatelessWidget {
-  const NoteWidget({super.key});
+  const NoteWidget({super.key, required this.title, required this.content});
+
+  final Stream<String> title;
+  final Stream<String> content;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.yellow[100],
+        border: Border.all(color: Colors.yellow[700]!),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: const Text(
+        'This is a note widget. It can be used to display important information or reminders to the user.',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    );
   }
 }
